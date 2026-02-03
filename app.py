de9647c1dca233aae08b9e63bcc8ccc22c72cea9
ernest <ernestanmol@gmail.com>
2026-02-03 09:06:19 +0530

    new profile

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# -----------------------
+# Routes
+# -----------------------
+
 @app.route("/")
 def home():
+    """Home page"""
     return render_template("index.html")
 
+
+# -----------------------
+# App Runner
+# -----------------------
 if __name__ == "__main__":
-    app.run(host='0.0.0.0' ,debug=True)
+    app.run(host="0.0.0.0", debug=True)
